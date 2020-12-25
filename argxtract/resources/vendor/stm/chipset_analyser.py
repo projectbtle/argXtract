b174bdd31277bfea634f0c434aedb827b5ba3b70
project.btle@tutanota.com <project.btle@tutanota.com>
2020-12-25 02:13:19 +0000

    STM

---
+++ argxtract/resources/vendor/stm/chipset_analyser.py
@@ -0,0 +1,13 @@
+class VendorChipsetAnalyser:
+    def __init__(self):
+        pass
+        
+    def test_binary_against_vendor(self):
+        return True
+        
+    def generate_output_metadata(self):
+        metadata = {}
+        return metadata
+        
+    def reset(self):
+        pass
